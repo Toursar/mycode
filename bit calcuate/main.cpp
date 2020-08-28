7bb4f810abacd08402aec96f4679d4e34f9cb92f
ZT <634041700@qq.com>
2020-08-28 11:00:03 +0800

    add ^operator

--- bit calcuate/main.cpp
+++ bit calcuate/main.cpp
@@ -18,15 +18,28 @@ int muti(int i, int j)
     }
     return res;
 }
-//除去数字n的最后一位1, n & (n - 1)
-void remove_last1(int n)
+//与算符
+void and_operator(int n)
 {
+    //除去数字n的最后一位1, n & (n - 1)
     cout << n << " 去除最后一位1后: " << (n & (n - 1)) << endl;
+    //n & (-n)得到最后一位1
+    cout << n << " 最后一位1是: " << (n & (-n)) << endl;
+}
+//异或操作,异或操作满足交换律和结合律
+//利用异或的这些性质，可以用来查找一组数据中未重复数据
+//A ^ B ^ B = A
+void XOR_operator(int n)
+{
+    //任何数与0异或不变，与自己异或为0
+    cout << "n ^ n = " << (n ^ n) << endl;
+    cout << "n ^ 0 = " << (n ^ 0) << endl;
 }
 
 int main()
 {
     cout << muti(5, 9) << endl;
-    remove_last1(7);
+    and_operator(6);
+    XOR_operator(12);
     return 0;
 }
